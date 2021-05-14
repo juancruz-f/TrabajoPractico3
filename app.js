98d05d69e1adf45a80e154aede1689dbc92dfe06
juancruz-f <juancruzfernandez108@gmail.com>
2021-05-14 12:20:14 -0300

    Codigo comentado

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const moduloTareas = require('./tareas');
-const process = require('process');
+const moduloTareas = require('./tareas');//requerimos nuestro modulo tareas
+const process = require('process');//Usamos el modulo nativo process
 
-const accion = process.argv[2];
+const accion = process.argv[2];//argv es una funcionalidad del modulo process que nos permite acceder a los elementos que escribimos en nuestra terminal, en este caso el comando será nuestro tercer elemento ya que tiene la posición 2.
 
-switch (accion) {
-    case 'agregar':
+switch (accion) {//Con switch ahorramos codigo y evitamos usar if y else.
+    case 'agregar'://si escribimos agregar en la posicion 2 de la terminal se ejecutará este codigo.
         let titulo = process.argv[3];
-        if(!titulo){
+        if(!titulo){//si no escribimos un titulo por consola nos devolverá el string "Debes escribir un título"
             console.log('Debes escribir un título');
             break;
         }
         let estado = process.argv[4];
         moduloTareas.agregarTarea(titulo,estado)
         break;
-    case 'listar':
+    case 'listar'://en cambio, al escribir listar en la posición 2 de la terminal, se ejecutará este codigo y nos listará las tareas.
         moduloTareas.listarTareas()
         break
     case 'filtrar':
         moduloTareas.filtrarTareas(process.argv[3])
         break
-    default:
+    default://El default sirve para que haya una respuesta por defecto, en este caso si no ponemos ninguna accion en la posicion 2 del argv, nos retorna por consola ese string.
         console.log("Debes ingresar una acción");
         break;
 }
--- tareas/index.js
+++ tareas/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const tareas = JSON.parse(fs.readFileSync('./db/tareas.json', 'utf-8'));
-module.exports = {
+const fs = require('fs');//requerimos el modulo filesystem
+const tareas = JSON.parse(fs.readFileSync('./db/tareas.json', 'utf-8'));//leemos y parseamos el json para poder utilizarlo con javascript
+module.exports = {//exportamos todo este modulo con sus funciones para poder utilizarlo en nuestro app
 
-    agregarTarea: function (titulo, estado = 'pendiente') {
+    agregarTarea: function (titulo, estado = 'pendiente') {//agrega una tarea nueva con push, lo guarda en el json con guardarJson que stringuifica ese codigo, y nos lista las tareas con las nuevas que hayamos agregado. Push es el metodo de array que usamos para agregarle elementos a ese array de tareas.
         let nuevaTarea = {
             titulo,
             estado
@@ -11,17 +11,17 @@ module.exports = {
         this.guardarJson(tareas);
         this.listarTareas();
     },
-    listarTareas: function () {
+    listarTareas: function () {//forEach recorre el array y por cada elemento nos devuelve el console.log
         tareas.forEach(tarea => {
             console.log(tarea);
         });
     },
-    filtrarTareas: function (filtro) {
+    filtrarTareas: function (filtro) {//Filter crea un array nuevo con los elementos que cumplen la condición de esa función. En este caso nos devuelve solo las tareas que incluyan la palabra filtro que sea el estado de esa tarea.
         let tareasFiltradas = tareas.filter(tarea => tarea.estado === filtro || tarea.titulo.includes(filtro));
         return console.log(tareasFiltradas)
     }
     ,
-    guardarJson: function (tareas) {
+    guardarJson: function (tareas) {//Aca usamos writefilesync para guardar el json modificado, stringify nos permite pasar a formato json.
         fs.writeFileSync('./db/tareas.json', JSON.stringify(tareas), 'utf-8')
     }
 }
